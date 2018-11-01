c3d206905dbc8c4b12f42ed325ed9211f9f7a0b8
Lei Xu <daniel_xulei@hotmail.com>
2018-10-31 23:29:40 -0700

    Make Material class working

---
+++ Material.h
@@ -0,0 +1,40 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+
+#include "Vectors.h"
+
+enum MaterialType {DIFF, SPEC, REFR };  // material types, used in radiance() 
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+class Material{
+private:
+	MaterialType m_type;
+	Vector3 m_color;
+	Vector3 m_emission;
+	//Texture tex;
+public:
+	Material(MaterialType t=DIFF, Vector3 c=Vector3(1,1,1), Vector3 e=Vector3(0,0,0));
+	MaterialType getType();
+	Vector3 getColor();
+	Vector3 getColorAt(double u, double v);
+	Vector3 getEmission();
+
+	
+};
+
+Material::Material(MaterialType t, Vector3 c, Vector3 e){
+	m_type = t;
+	m_color = c;
+	m_emission = e;
+}
+
+MaterialType Material::getType(){return m_type;}
+Vector3 Material::getColor(){return m_color;}
+Vector3 Material::getEmission() {return m_emission;}
+//Vector3 getColorAt(double u, double v) const;
+
+
+#endif //MATERIAL_H
---
+++ Triangle.h
@@ -0,0 +1,18 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include "Vectors.h"
+
+
+
+class Triangle: public Shape{
+private:
+
+// Triangle Public Methods
+public:
+
+
+
+
+
+#endif// TRIANGLE_H
