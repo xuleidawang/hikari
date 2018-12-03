4d443035c3f7e5ae34929fd1ece5a5413aa72194
Lei Xu <daniel_xulei@hotmail.com>
2018-12-03 15:43:23 -0800

    Initialize BRDF & BTDF class

--- Reflection.h
+++ Reflection.h
@@ -12,9 +12,9 @@ public:
     BxDF(MaterialType type) : type(type) {}
     //bool MatchesFlags(MaterialType t) const { return (type & t) == type; }
     virtual Vector3 f(const Vector3 &wo, const Vector3 &wi) const = 0;
-    virtual Vector3 Sample_f(const Vector3 &wo, Vector3 *wi, const Point2f &sample, double *pdf, MaterialType *sampledType = nullptr) const;
+    virtual Vector3 Sample_f(const Vector3 &wo, Vector3 *wi, const Vector2 &sample, double *pdf, MaterialType *sampledType = nullptr) const;
     virtual Vector3 rho(const Vector3 &wo, int nSamples, const Vector2 *samples) const;
-    virtual Vector3 rho(int nSamples, const Point2f *samples1, const Point2f *samples2) const;
+    virtual Vector3 rho(int nSamples, const Vector2 *samples1, const Vector2 *samples2) const;
     virtual double Pdf(const Vector3 &wo, const Vector3 &wi) const;
     virtual std::string ToString() const = 0;
 
