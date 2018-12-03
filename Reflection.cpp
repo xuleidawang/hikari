d18fb37b42c870ea1051d7c7a63fb1ab6b1473ff
Lei Xu <daniel_xulei@hotmail.com>
2018-12-03 11:28:38 -0800

    Initialize Reflection class

---
+++ Reflection.cpp
@@ -0,0 +1,7 @@
+//
+// Created by LEI XU on 12/3/18.
+//
+#include "Reflection.h"
+
+
+
---
+++ Reflection.h
@@ -0,0 +1,41 @@
+//
+// Created by LEI XU on 12/3/18.
+//
+
+#ifndef PATHTRACER_REFLECTION_H
+#define PATHTRACER_REFLECTION_H
+
+#include "Material.h"
+class BxDF {
+public:
+    virtual ~BxDF(){}
+    BxDF(MaterialType type) : type(type) {}
+    //bool MatchesFlags(MaterialType t) const { return (type & t) == type; }
+    virtual Vector3 f(const Vector3 &wo, const Vector3 &wi) const = 0;
+    virtual Vector3 Sample_f(const Vector3 &wo, Vector3 *wi, const Point2f &sample, double *pdf, MaterialType *sampledType = nullptr) const;
+    virtual Vector3 rho(const Vector3 &wo, int nSamples, const Vector2 *samples) const;
+    virtual Vector3 rho(int nSamples, const Point2f *samples1, const Point2f *samples2) const;
+    virtual double Pdf(const Vector3 &wo, const Vector3 &wi) const;
+    virtual std::string ToString() const = 0;
+
+    // BxDF Public Data
+    const MaterialType type;
+};
+
+
+class BRDF: public BxDF{
+
+};
+
+
+class BTDF: public  BxDF{
+
+};
+
+
+
+
+
+
+
+#endif //PATHTRACER_REFLECTION_H
