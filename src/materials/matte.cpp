8150a8aea359bfaef37716da366ed2822136f1ef
Lei Xu <daniel_xulei@hotmail.com>
2020-06-08 21:47:59 -0700

    Fix null reference in MatteMaterial::ComputeScatteringFunctions.

--- src/materials/matte.cpp
+++ src/materials/matte.cpp
@@ -3,6 +3,7 @@
 namespace hikari
 {
     void MatteMaterial::ComputeScatteringFunctions(Intersection* si, bool allowMultipleLobes) const {
+        si->bsdf = new BSDF(*si);
         Vector3 r = kd;
         LambertianReflection lambertianReflection(r);
         si->bsdf->Add(&lambertianReflection);
